2ab19196610817ffe26c7410434dea35afb65d7e
DetectiveSquirrel <ratman7539@windowslive.com>
2023-10-09 21:11:48 +1300

    fix refs

--- Guardians-R-UsSettings.cs
+++ Guardians-R-UsSettings.cs
@@ -7,10 +7,5 @@ namespace Guardians_R_Us
     {
         //Mandatory setting to allow enabling/disabling your plugin
         public ToggleNode Enable { get; set; } = new ToggleNode(false);
-
-        //Put all your settings here if you can.
-        //There's a bunch of ready-made setting nodes,
-        //nested menu support and even custom callbacks are supported.
-        //If you want to override DrawSettings instead, you better have a very good reason.
     }
 }
